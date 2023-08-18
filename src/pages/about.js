8e56cd6f1d21f25348ae83f058e0b64c7a8e3b49
2derp4you <iam2derp4you@gmail.com>
2023-08-19 00:23:32 +0200

    done, for now v1

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,10 @@ const About = () => {
                 <div className="about-main-header">
                     About
                 </div>
+                <h2>Made by <a href="https://github.com/2derp4you">2derp4you/SigveDev</a></h2>
+                <h3>Weather API: yr.no/met.no</h3>
+                <h3>Travel API: entur.no</h3>
+                <h3>News API: nrk.no</h3>
             </div>
         </div>
     )
